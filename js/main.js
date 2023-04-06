55dcbb1d7c807a74981704c056a5292c9c963f2b
nadiras <nadiraomar98@gmail.com>
2023-04-06 18:20:33 -0500

    colors display in guess and win-sequence is set

--- js/main.js
+++ js/main.js
@@ -2,15 +2,15 @@
   const userGuess = [0,1,2,3,4,5];
   const colors = ['blue','pink','yellow','green','red','orange'];
   const numOfGuesses = 4;
-  const totalAttempts = 10;
 
   /*----- state variables -----*/
-  let attemptNum; // attmept number initialized to 1
-  let win; // win sequence initialized to random 
-  let currGuess; // current guess 
+  let circleToFill;
+  let rowToFill; // attmept number initialized to 9
+  let winColorToFill; // win sequence initialized to random 
+  let rowToCompare = []; // current guess 
   /*----- cached elements  -----*/
-  const playBtnEl = document.getElementById('playBtn');
-  const colorEl = document.querySelectorAll('.colors > div');
+  const restartBtnEl = document.getElementById('restartBtn');
+  const colorEl = document.querySelector('.colors');
   const winEl = document.querySelectorAll('.secret-color');
   const hintEl = document.querySelectorAll('.hint');
   const boardEl = document.querySelector('#board');
@@ -20,29 +20,46 @@
   const colorRowEl = document.querySelectorAll('.colors');
 
 
-//   currAttemptEl[attemptNum-1].querySelectorAll('.colors')
-
   /*----- event listeners -----*/
-  playBtnEl.addEventListener('click', function handleClick(evt){
-        //when the playBtn is clicked update attemptNum to 1st attempt
-        //update the currGuess to 1st attempt 1st guess
-        //update the win to a random sequence of colors
-        attemptNum = totalAttempts -1;
+  restartBtnEl.addEventListener('click', function handleClick(evt){
+       // when this button is clicked , gameboard should clear colortofill 
+       // and rowtofill and hints
+       initialize();
 
-       let rowDivs = [...currAttemptEl[attemptNum].querySelectorAll('.guess')];
-        
-       rowDivs.forEach(function(rowDiv){
-            rowDiv.style.backgroundColor = colors[0];
-      })
-      
-
-
-        // evt.target.style.visibility = 'hidden'
-
-
-
-        return rowDivs
   })
+
+  colorEl.addEventListener('click', function pickColor(evt){
+      const clickedEl = evt.target;
+
+      if (clickedEl.tagName !== 'DIV') return;
+
+      if (circleToFill === 5){
+            rowToCompare = [];
+            circleToFill = 1;
+            rowToFill -= 1;
+      }
+
+
+      currAttemptEl[rowToFill].children[circleToFill].style.backgroundColor = clickedEl.id
+      circleToFill += 1;
+
+      rowToCompare.push(clickedEl.id)
+       
+      if(rowToCompare.length === 4){
+            let count = 0;
+            for (let i = 0; i < 4; i++){
+                  if (rowToCompare[i] === winColorToFill[i]){
+                        count +=1;
+                        //update hints 
+                  }
+            }
+            if (count === 4){
+                  alert('you won')
+            }
+      }
+
+      
+  });
 
 
   /*----- functions -----*/
@@ -50,14 +67,26 @@
   initialize();
   //used to initialize all state variables
   function initialize() {
-      attemptNum = null;
-      win = getWin();
+      rowToFill = 9;
+      circleToFill = 1;
+      winColorToFill = [];
+      winColorToFill = getWin();
+      console.log(winColorToFill)
       currGuess = [];
-
+      // put win logic 
       //render();
   }
 
   function getWin(){
+
+      for(let i = 0; i < 4; i++){
+            winColorToFill.push(colors[Math.floor(Math.random() * 5)])
+      }
+
+      return winColorToFill
+  }
+
+  function renderWin() {
 
   }
 
@@ -67,6 +96,8 @@
 
 //   }
 
-function guessCode() {
-      
-}
+// function guessCode() {
+
+// }
+
+
