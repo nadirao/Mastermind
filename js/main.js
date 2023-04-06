6d21f3f4de47b0ced8b1d478e1b0ff368cc1a64b
nadiras <nadiraomar98@gmail.com>
2023-04-06 16:01:30 -0500

    added code to js

--- js/main.js
+++ js/main.js
@@ -11,15 +11,38 @@
   /*----- cached elements  -----*/
   const playBtnEl = document.getElementById('playBtn');
   const colorEl = document.querySelectorAll('.colors > div');
-  const guessEl = document.querySelectorAll('.guess');
   const winEl = document.querySelectorAll('.secret-color');
   const hintEl = document.querySelectorAll('.hint');
   const boardEl = document.querySelector('#board');
   const currAttemptEl = document.querySelectorAll('.row');
+  const guessEl = document.querySelectorAll('.guess');
   const winRowEl = document.querySelectorAll('.win-sequence');
   const colorRowEl = document.querySelectorAll('.colors');
 
+
+//   currAttemptEl[attemptNum-1].querySelectorAll('.colors')
+
   /*----- event listeners -----*/
+  playBtnEl.addEventListener('click', function handleClick(evt){
+        //when the playBtn is clicked update attemptNum to 1st attempt
+        //update the currGuess to 1st attempt 1st guess
+        //update the win to a random sequence of colors
+        attemptNum = totalAttempts -1;
+
+       let rowDivs = [...currAttemptEl[attemptNum].querySelectorAll('.guess')];
+        
+       rowDivs.forEach(function(rowDiv){
+            rowDiv.style.backgroundColor = colors[0];
+      })
+      
+
+
+        // evt.target.style.visibility = 'hidden'
+
+
+
+        return rowDivs
+  })
 
 
   /*----- functions -----*/
@@ -27,16 +50,23 @@
   initialize();
   //used to initialize all state variables
   function initialize() {
-      attemptNum = 1;
+      attemptNum = null;
       win = getWin();
       currGuess = [];
-        render();
+
+      //render();
   }
 
   function getWin(){
 
   }
 
-  function render() {
 
-  }
+  //responsible for transferring all state variables to the dom
+//   function render() {
+
+//   }
+
+function guessCode() {
+      
+}
